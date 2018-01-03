0ae66965b10e2977a76720b390d648282da73b9a
jacknic <jackoktea@gmail.com>
2018-01-03 12:05:11 +0800

    小幅度修改fragment栈管理

--- app/src/main/java/com/jacknic/glut/view/fragment/setting/ColorsDialogFragment.java
+++ app/src/main/java/com/jacknic/glut/view/fragment/setting/ColorsDialogFragment.java
@@ -6,14 +6,19 @@ import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.jacknic.glut.MainActivity;
 import com.jacknic.glut.R;
 import com.jacknic.glut.adapter.ColorsSelectorAdapter;
+import com.jacknic.glut.page.HomePage;
+import com.jacknic.glut.page.SettingPage;
+import com.jacknic.glut.stacklibrary.StackManager;
 import com.jacknic.glut.util.Config;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -22,7 +27,7 @@ import static android.content.Context.MODE_PRIVATE;
  * 设置颜色页弹窗
  */
 
-public class ColorsDialogFragment extends DialogFragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+public class ColorsDialogFragment extends DialogFragment implements AdapterView.OnItemClickListener {
 
     public static void launch(Activity context) {
         Fragment fragment = context.getFragmentManager().findFragmentByTag("ColorsDialogFragment");
@@ -41,8 +46,6 @@ public class ColorsDialogFragment extends DialogFragment implements AdapterView.
         GridView gridView = (GridView) view.findViewById(R.id.grid_colors);
         gridView.setAdapter(new ColorsSelectorAdapter(this));
         gridView.setOnItemClickListener(this);
-        gridView.setOnItemLongClickListener(this);
-
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .setNegativeButton(android.R.string.cancel, null)
@@ -52,16 +55,27 @@ public class ColorsDialogFragment extends DialogFragment implements AdapterView.
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         SharedPreferences setting = getActivity().getSharedPreferences(Config.PREFER_SETTING, MODE_PRIVATE);
-        String which = Config.SETTING_THEME_INDEX;
-        setting.edit().putInt(which, position).putBoolean(Config.IS_REFRESH, true).apply();
+        setting.edit().putInt(Config.SETTING_THEME_INDEX, position).putBoolean(Config.IS_REFRESH, true).apply();
+        getActivity().setTheme(Config.THEME_LIST[position]);
         ImageView iv_select_theme_color = (ImageView) getActivity().findViewById(R.id.setting_iv_select_theme_color);
         int theme_index = setting.getInt(Config.SETTING_THEME_INDEX, Config.SETTING_THEME_COLOR_INDEX);
-        iv_select_theme_color.setImageResource(Config.COLORS[theme_index]);
+        int color_res = Config.COLORS[theme_index];
+        iv_select_theme_color.setImageResource(color_res);
+        MainActivity activity = (MainActivity) getActivity();
+        android.support.v7.app.ActionBar actionBar = activity.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setBackgroundDrawable(getResources().getDrawable(color_res));
+            StackManager stackManager = activity.manager;
+            HomePage homePage = new HomePage();
+            stackManager.setFragment(homePage);
+            FragmentManager fragmentManager = activity.getSupportFragmentManager();
+            SettingPage settingPage = new SettingPage();
+            fragmentManager.beginTransaction()
+                    .add(homePage, homePage.getClass().getName())
+                    .add(settingPage, settingPage.getClass()
+                            .getName())
+                    .commit();
+        }
         dismiss();
-    }
-
-    @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        return true;
     }
 }
