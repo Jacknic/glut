aa2e3c49e23e053a052cd994145d556c149e9dea
Jacknic <jacknic@qq.com>
2020-03-26 18:13:33 +0800

    简单处理 Cookie path 问题

--- app/src/main/java/com/jacknic/glut/data/net/AndroidCookieJar.kt
+++ app/src/main/java/com/jacknic/glut/data/net/AndroidCookieJar.kt
@@ -1,15 +1,14 @@
 package com.jacknic.glut.data.net
 
 import android.webkit.CookieManager
+import com.jacknic.glut.data.util.URL_GLUT_CW
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
 
 /**
- * Android 设备 Cookie存储
- *<p>
- * 用于 Cookie 同步
- * </p
+ * Cookie同步存储
+ *
  * @author Jacknic
  */
 class AndroidCookieJar : CookieJar {
@@ -19,13 +18,20 @@ class AndroidCookieJar : CookieJar {
     override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
         val cookieValue = StringBuilder()
         cookies.joinTo(cookieValue, ";", transform = { "${it.name()}=${it.value()}" })
-        cookieManager.setCookie(url.toString(), cookieValue.toString())
+        //Logger.d("存储：${url}->${cookieValue}")
+        // 简单处理财务 Cookie path 问题
+        var urlValue = url.toString()
+        if (urlValue.startsWith(URL_GLUT_CW)) {
+            urlValue = url.host()
+        }
+        cookieManager.setCookie(urlValue, cookieValue.toString())
         cookieManager.flush()
     }
 
     override fun loadForRequest(url: HttpUrl): List<Cookie> {
         val cookieStringValue = cookieManager.getCookie(url.toString()).orEmpty()
         val valueList = cookieStringValue.split(";")
+        //Logger.d("读取：${url}->${cookieStringValue}")
         return valueList.mapNotNull { Cookie.parse(url, it) }
     }
 }
